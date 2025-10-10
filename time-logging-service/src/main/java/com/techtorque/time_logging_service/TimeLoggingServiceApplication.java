698bd68feff895ee27ebf257e92bbffc24ac7ec9
RandithaK <bharkula3@gmail.com>
2025-10-11 00:22:30 +0530

    feat: Update SecurityConfig to disable form login and HTTP Basic authentication, and add Swagger/OpenAPI whitelist

--- time-logging-service/src/main/java/com/techtorque/time_logging_service/TimeLoggingServiceApplication.java
+++ time-logging-service/src/main/java/com/techtorque/time_logging_service/TimeLoggingServiceApplication.java
@@ -2,8 +2,9 @@ package com.techtorque.time_logging_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class TimeLoggingServiceApplication {
 
 	public static void main(String[] args) {
