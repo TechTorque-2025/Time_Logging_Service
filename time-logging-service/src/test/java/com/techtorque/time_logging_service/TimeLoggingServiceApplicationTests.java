e1279c3048ffc1f44b0feb457dec4eeb8fe3867a
RandithaK <bharkula3@gmail.com>
2025-11-05 21:27:18 +0530

    test: Update application tests

--- time-logging-service/src/test/java/com/techtorque/time_logging_service/TimeLoggingServiceApplicationTests.java
+++ time-logging-service/src/test/java/com/techtorque/time_logging_service/TimeLoggingServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.techtorque.time_logging_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TimeLoggingServiceApplicationTests {
 
 	@Test
